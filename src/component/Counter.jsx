3dec50859177133b61f8b7913de78040f4d36423
Srijan Bista <Srijanbista4284@gmail.com>
2022-07-20 22:55:48 +0545

    color updated, count status set to titlebar

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,30 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import './styles/Counter.css'
+import React, { useEffect, useState } from "react";
+import "./styles/Counter.css";
 function Counter() {
+  let [count, setCount] = useState(0);
 
-
-
-
-    let [count, setCount] = useState(0);
-    // useEffect(() => {
-    //     document.title.innerHTML = { count };
-    // }, [count])
-    return (
-
-        <div className="background">
-            <div className='new'>
-                <h1>Counter</h1>
-                <div className="counter">
-                    <button onClick={() => { count ? setCount(--count) : setCount(count) }} disabled={!count}>-</button>
-                    <h1>{count}</h1>
-                    <button onClick={() => { setCount(count + 1) }}>+</button>
-                </div>
-                <div className="reset">
-                    <button onClick={() => { setCount(0) }}>Reset</button>
-                </div>
-            </div>
+  document.getElementById("title").innerHTML = `Count-${count}`;
+  return (
+    <div className="background">
+      <div className="new">
+        <h1>Counter</h1>
+        <div className="counter">
+          <button
+            onClick={() => {
+              count ? setCount(--count) : setCount(count);
+            }}
+            disabled={!count}
+          >
+            -
+          </button>
+          <h1>{count}</h1>
+          <button
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            +
+          </button>
         </div>
-    )
+        <div className="reset">
+          <button
+            onClick={() => {
+              setCount(0);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Counter
+export default Counter;
